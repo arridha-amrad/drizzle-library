1112f5223a09e3a45c45fd0269f1a61edb1459cd
Arridha Amrad <arridhaamrad@gmail.com>
2024-05-23 13:44:52 +0700

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="">
-      <button className="btn btn-primary">Button</button>
+    <main className="flex items-center justify-center min-h-screen">
+      <div>
+        <h1 className="text-2xl font-semibold">Welcome to Library</h1>
+      </div>
     </main>
   );
 }
